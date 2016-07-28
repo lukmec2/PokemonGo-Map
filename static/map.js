d159e9def4033683505030aa96d1500e2dfc4407
lenix2 <lenny.r@live.de>
2016-07-28 10:38:04 +0200

    Update map.js

--- static/map.js
+++ static/map.js
@@ -704,8 +704,9 @@ function getColorByDate(value){
 function setupScannedMarker(item) {
     var circleCenter = new google.maps.LatLng(item.latitude, item.longitude);
 
- var offsetlng = 0.0012775;
- console.log(circleCenter.lng());
+ //var offsetlng = 0.0012775;
+ var offsetlng = 0.00128090842;
+ console.log(circleCenter.lat());
  var flightPlanCoordinates = [
     {lat: circleCenter.lat() + 0.0009, lng:  circleCenter.lng()},
     {lat: circleCenter.lat() + 0.00045, lng: circleCenter.lng() -offsetlng},
