db2e7af73bc021aee54d140fe7dbd4bb997f0753
lenix2 <lenny.r@live.de>
2016-07-28 13:27:09 +0200

    Update map.js

--- static/map.js
+++ static/map.js
@@ -705,7 +705,7 @@ function setupScannedMarker(item) {
     var circleCenter = new google.maps.LatLng(item.latitude, item.longitude);
  //var offsetlng = 0.0012775;
  //var offsetlng = 0.001280908;
-   var offsetlng = 0.0012805;
+   var offsetlng = 0.001280;
  console.log(circleCenter.lat());
  var flightPlanCoordinates = [
     {lat: circleCenter.lat() + 0.0009, lng:  circleCenter.lng()},
